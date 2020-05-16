5d616f416685a709de73e6e32dfc9b7f8618ad50
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-05-16 11:02:06 -0500

    Psuedo code for back end functionality and some notes

---
+++ seatingLogic.js
@@ -0,0 +1,30 @@
+//HAWT Dawna
+//10 booths which seat 6
+//10 tables in the Resteraunt each table seats 4.
+// if we combine a table to seat 8 we now have 9 tables but same amount(40) people allowed + 60 ppl booths
+//allowing 100 guests
+
+//if all seats are taken then inject "partyName" and "partySize"
+
+
+/* 
+The user form should
+Ask them *partySize *partyTime *contactPhone *contactEmail *Special notes or allergies
+
+ONLY VARIABLES THAT MATTER ARE *partySize & *partyTime
+Does the resteraunt have enough seating during the time requested? if yes then give them the reservation and subtract the partySize
+from the available seating total.
+
+if no seats available at that time then do not show the option to choose. but offer 20 minutes before/after.
+if 4 reservations for a certain time then we should subtract that many from the total seats available.
+**Either only show available times or 
+if user asks for a time that  is filled  the*hasn't occures ye
+WHEN USER SUBMITS FORM GIVE THE USER ------------------------------------------------------
+*What place they are in line (without revealing the names of people around them)
+*Average wait time or most likely time to wait.
+
+*when it is their turn then they will recieve a call or email.
+
+
+
+*/
