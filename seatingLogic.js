a9c1caa42376c089069d108a160a67c38b11204a
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-05-16 11:11:40 -0500

    Added more comments

--- seatingLogic.js
+++ seatingLogic.js
@@ -6,25 +6,27 @@
 
 //if all seats are taken then inject "partyName" and "partySize"
 
-
 /* 
 The user form should
 Ask them *partySize *partyTime *contactPhone *contactEmail *Special notes or allergies
 
 ONLY VARIABLES THAT MATTER ARE *partySize & *partyTime
 Does the resteraunt have enough seating during the time requested? if yes then give them the reservation and subtract the partySize
-from the available seating total.
+from the available seating total for that time.
 
 if no seats available at that time then do not show the option to choose. but offer 20 minutes before/after.
-if 4 reservations for a certain time then we should subtract that many from the total seats available.
+if 4 reservations for a certain time then we should subtract (tableSize * amountOfpartys) from the total seats available.
 **Either only show available times or 
-if user asks for a time that  is filled  the*hasn't occures ye
+if time has already occured than it is not available.
 WHEN USER SUBMITS FORM GIVE THE USER ------------------------------------------------------
 *What place they are in line (without revealing the names of people around them)
+1.Johnazues (fam of 4) Your wait time is the shortest!
+2.spangler (group of 8) Your wait time is roughly 20min
+3.lookeer (couple of 2) Your wait time is roughly 23min
+
 *Average wait time or most likely time to wait.
-
 *when it is their turn then they will recieve a call or email.
-
+**Eventually add option to Cancle or Change inputs
 
 
 */
